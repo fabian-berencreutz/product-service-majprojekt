f4e06d06b8e370b65e16b3f99e463ce822127c4a
fabian-berencreutz <fabianberencreutz@gmail.com>
2026-05-19 11:18:38 +0200

    fix database etc

--- src/test/java/se/iths/fabian/productservicemajprojekt/ProductServiceMajprojektApplicationTests.java
+++ src/test/java/se/iths/fabian/productservicemajprojekt/ProductServiceMajprojektApplicationTests.java
@@ -2,14 +2,9 @@ package se.iths.fabian.productservicemajprojekt;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 @SpringBootTest
 class ProductServiceMajprojektApplicationTests {
-
-    @MockitoBean
-    private JwtDecoder jwtDecoder;
 
     @Test
     void contextLoads() {
